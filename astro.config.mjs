aa62c73cf103e30b38f8e0ab3e9832848952c286
ojack <ojackson@gmail.com>
2026-04-10 19:57:45 +0200

    fix: add redirect so /admin serves CMS admin page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://warrenjackson.com',
+  redirects: {
+    '/admin': '/admin/index.html',
+  },
 });
